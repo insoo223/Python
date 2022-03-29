de482f54401ebc99195a9b8f235c2eeae22cc875
Insoo Kim <insoo@hotmail.com>
2022-03-29 07:57:41 +0000

    Number operations

---
+++ numbers.py
@@ -0,0 +1,11 @@
+# A strange number will be shown up representing internal presentation mode.
+print(0.2 + 0.1)
+print(2**3)
+
+# Underscore within number is ignored
+universe_age = 14_000_000_000
+print(universe_age)
+
+# Multiple assignments
+x,y,z = 2,3,4
+print(x*y*z)
